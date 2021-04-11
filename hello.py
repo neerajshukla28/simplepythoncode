5d9cb0107fcf8059a2d164767981f32675728043
neerajshukla28 <neerajshukla28@gmail.com>
2021-04-11 19:08:02 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ from azureml.core.datastore import Datastore
 
    datastore = Datastore(workspace="Playground", name="workspaceblobstore")
 
-   schedule = Schedule.create(workspace, name="TestSchedule", pipeline_id="41858f09-dc0a-4191-a97f-de963fa277ca"
+   schedule = Schedule.create(workspace, name="TestSchedule", pipeline_id="3100e87c-3300-400b-a5a5-470e85a100b3"
                               experiment_name="working version", datastore=datastore,
                               polling_interval=25, path_on_datastore="file/path")
